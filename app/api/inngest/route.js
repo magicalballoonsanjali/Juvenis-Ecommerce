e432e183a9c95b29ef3d2f3040244992b0fe87f8
Your Name <your-email@example.com>
2025-09-22 12:51:57 +0530

    inngest integration

---
+++ app/api/inngest/route.js
@@ -0,0 +1,10 @@
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import { helloWorld } from "../../../inngest/functions";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,syncUserUpdation,syncUserDeletion
+  ],
+});
