34a247caae7c4ec75d18f2092cd547c32e59f9f4
Your Name <your-email@example.com>
2025-09-23 15:07:14 +0530

    final command

--- models/User.js
+++ models/User.js
@@ -5,10 +5,14 @@ const userSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     name:{ type:String, required:true},
     email:{type:String,required:true,unique:true},
-    imageURL:{ type:String, required:true},
+    imageURL:{ type:String, required: false, default: ""},
     cartItems:{type:Object,default:{}},
     
 },{minimize:false});
+
+if (mongoose.models.user) {
+  delete mongoose.models.user;
+}
 
 const User = mongoose.models.user || mongoose.model("user",userSchema);
 
