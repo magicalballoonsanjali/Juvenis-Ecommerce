525dfd2f98ad824f58c1d7d4bf3b01bfd4137ef7
Your Name <your-email@example.com>
2025-09-23 11:51:35 +0530

    re-solved

--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-import { unique } from "next/dist/build/utils";
+
 
 const userSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     name:{ type:String, required:true},
     email:{type:String,required:true,unique:true},
     imageURL:{ type:String, required:true},
-    cartItems:{type:String,required:true},
+    cartItems:{type:Object,default:{}},
     
 },{minimize:false});
 
