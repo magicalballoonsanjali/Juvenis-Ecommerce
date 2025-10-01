37a6fc1f70a61dcb3c4433835fd418ad5dd77271
Your Name <your-email@example.com>
2025-10-01 12:32:20 +0530

    final changes

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -1,15 +1,14 @@
 
 import Aboutus from '@/components/Aboutus'
-import Footer from '@/components/Footer'
-import Navbar from '@/components/Navbar'
+
 import React from 'react'
 
 const page = () => {
   return (
     <>
-    <Navbar/>
+
     <Aboutus/>
-    <Footer/>
+
     
    
     </>
--- components/Aboutus.jsx
+++ components/Aboutus.jsx
@@ -1,10 +1,12 @@
 'use client';
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const Aboutus = () => {
   return (
     <div>
-      
+      <Navbar/>
        <div className='flex flex-col-reverse md:flex-row justify-center items-center p-8 md:px-20 gap-2 '>
         <div className='md:w-1/2 flex flex-col gap-3 '>
         <h1 className='text-4xl py-4'>About Us</h1>
@@ -19,7 +21,7 @@ const Aboutus = () => {
                         </div>
         </div>
     </div>
-
+    <Footer/>
     </div>
     
   )
