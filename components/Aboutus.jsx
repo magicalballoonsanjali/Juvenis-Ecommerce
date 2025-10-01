fbaaf2ce0fda9b31354d7a242e7b296a442a5fed
Your Name <your-email@example.com>
2025-10-01 12:42:12 +0530

    final changes

--- components/Aboutus.jsx
+++ components/Aboutus.jsx
@@ -6,7 +6,6 @@ import React from 'react'
 const Aboutus = () => {
   return (
     <div>
-      <Navbar/>
        <div className='flex flex-col-reverse md:flex-row justify-center items-center p-8 md:px-20 gap-2 '>
         <div className='md:w-1/2 flex flex-col gap-3 '>
         <h1 className='text-4xl py-4'>About Us</h1>
@@ -21,7 +20,7 @@ const Aboutus = () => {
                         </div>
         </div>
     </div>
-    <Footer/>
+
     </div>
     
   )
